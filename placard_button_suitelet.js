fa5c64c767bfd714657ca37407f19f85989bd9c6
Elizabeth Pursell <emp520@lehigh.edu>
2022-06-28 13:45:33 -0400

    Update placard_button_suitelet.js

--- placard_button_suitelet.js
+++ placard_button_suitelet.js
@@ -19,29 +19,29 @@ define(['N/render', 'N/record'], function(render, record){
     * @function onRequest
     */
   function onRequest(context) {
-      var custom_id = context.request.parameters.custom_id;
-      var renderer = render.create();
-      renderer.addRecord({
-        templateName: 'record',
-        record: record.load({
-          type: record.Type.ITEM_FULFILLMENT,
-          id: custom_id
-        })
-      });
-      renderer.setTemplateByScriptId("CUSTTMPL_123_7232941_610");
-      context.response.addHeader({
-	  	name: 'Content-Type:',
-	  	value: 'application/pdf'
+    var custom_id = context.request.parameters.custom_id;   //gets internal id of current record from CS program
+    var renderer = render.create();
+    renderer.addRecord({              //adds current record to renderer
+      templateName: 'record',
+      record: record.load({
+        type: record.Type.ITEM_FULFILLMENT,
+        id: custom_id
+      })
+    });
+    renderer.setTemplateByScriptId("CUSTTMPL_123_7232941_610");     //sets renderer's template as shipping placard html template
+    context.response.addHeader({
+	    name: 'Content-Type:',
+	    value: 'application/pdf'
 	  });
 	  context.response.addHeader({
 	  	name: 'Content-Disposition',
 	  	value: 'inline; filename="shippingplacard.pdf"'
 	  });
 
-	  renderer.renderPdfToResponse(context.response);
-      //context.response.writeFile(renderer.renderAsPdf());		//for download
+	  renderer.renderPdfToResponse(context.response);     //opens rendered pdf in new tab
+    //context.response.writeFile(renderer.renderAsPdf());		//for download
   }
   return {
-      onRequest: onRequest
+    onRequest: onRequest
   }
-  });
+});
