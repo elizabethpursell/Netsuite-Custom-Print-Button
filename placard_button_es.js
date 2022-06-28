3dc202864d0d33733a4b510e71961de998aab4f1
Elizabeth Pursell <emp520@lehigh.edu>
2022-06-28 13:45:00 -0400

    Update placard_button_es.js

--- placard_button_es.js
+++ placard_button_es.js
@@ -1,38 +1,38 @@
 define([], function(){
-/**
-  * 
-  * @NApiVersion 2.x
-  * @NScriptType UserEventScript
-  * @appliedtorecord itemfulfillment
-  */
-var exports = {};
-/**
-  * <code>beforeLoad</code> event handler
-  * 
-  * @gov 0
-  * 
-  * @param context
-  *        {Object}
-  * @param context.newRecord
-  *        {record} the new record being loaded
-  * @param context.type
-  *        {UserEventType} the action that triggered this event
-  * @param context.form
-  *        {form} The current UI form
-  * 
-  * @return {void}
-  * 
-  * @static
-  * @function beforeLoad
-  */
-function beforeLoad(context) {
-    context.form.addButton({
-        id: "custpage_printplacard",
-        label: "Print Placard",
-        functionName: "onButtonClick"
+  /**
+    * 
+    * @NApiVersion 2.x
+    * @NScriptType UserEventScript
+    * @appliedtorecord itemfulfillment
+    */
+  var exports = {};
+  /**
+    * <code>beforeLoad</code> event handler
+    * 
+    * @gov 0
+    * 
+    * @param context
+    *        {Object}
+    * @param context.newRecord
+    *        {record} the new record being loaded
+    * @param context.type
+    *        {UserEventType} the action that triggered this event
+    * @param context.form
+    *        {form} The current UI form
+    * 
+    * @return {void}
+    * 
+    * @static
+    * @function beforeLoad
+    */
+  function beforeLoad(context) {
+    context.form.addButton({          //adds button to UI that executes function "onButtonClick" from CS program
+      id: "custpage_printplacard",
+      label: "Print Placard",
+      functionName: "onButtonClick"
     });
-    context.form.clientScriptModulePath = "SuiteScripts/placard_button_click_cs.js";      //CS for placard button
-}
-exports.beforeLoad = beforeLoad;
-return exports;
+    context.form.clientScriptModulePath = "SuiteScripts/placard_button_click_cs.js";      //sets path to CS program for placard button
+  }
+  exports.beforeLoad = beforeLoad;
+  return exports;
 });
